d521a1bf8bf7e58fcf163a14f3c16ba6fabc7f93
Basa Gnana Chandrika <154501301+gnanachandrik@users.noreply.github.com>
2025-07-16 15:19:31 +0530

    Add files via upload

---
+++ phonePay.java
@@ -0,0 +1,7 @@
+package structuralAdapter.concreteClasses;
+
+public class phonePay {
+    public void makePay(int amount){
+        System.out.println("Payment completed through PhonePay: "+amount);
+    }
+}
---
+++ razorPay.java
@@ -0,0 +1,7 @@
+package structuralAdapter.concreteClasses;
+
+public class razorPay {
+    public void makePay(int amount){
+        System.out.println("Payment completed through RazorPay: "+amount);
+    }
+}
